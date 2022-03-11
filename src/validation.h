987a2aa0009b6c797f78bd3f1d10882cf57699ea
motty <fujicoin@yahoo.com>
2022-03-11 22:34:16 +0900

    fix compile error

--- src/validation.h
+++ src/validation.h
@@ -693,6 +693,9 @@ public:
     bool IsInitialBlockDownload() const;
     static int IsStartUp;
 
+    /** Find the last common block of this chain and a locator. */
+    CBlockIndex* FindForkInGlobalIndex(const CBlockLocator& locator) const EXCLUSIVE_LOCKS_REQUIRED(cs_main);
+
     /**
      * Make various assertions about the state of the block index.
      *
